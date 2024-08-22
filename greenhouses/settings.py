51da2021027bd621b8f84f94dea0a75a80f4d391
Layla El Ghoul <147739990+Scarletwtc@users.noreply.github.com>
2024-08-22 21:10:24 +0300

    Update settings.py

--- greenhouses/settings.py
+++ greenhouses/settings.py
@@ -120,7 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+
+import os
+
+# Define the directory where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure that you have STATIC_URL set as well
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
